781df41471d5628a09face3a4a7c8d72be591c16
jean carlos <jeancarlos3102@gmail.com>
2024-04-10 14:04:24 -0400

    refactoring css for shopping list

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,13 +2,25 @@ import './css/input.css';
 type InputProps = {
 	type: string;
 	placeholder?: string;
+	value?: number | string;
+	onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void;
+	readonly?: boolean;
 };
 
-const Input = ({ placeholder, type }: InputProps) => {
+const Input = ({
+	placeholder,
+	type,
+	value,
+	onChange,
+	readonly,
+}: InputProps) => {
 	return (
 		<>
 			<div className="group">
 				<input
+					readOnly={readonly}
+					onChange={onChange}
+					value={value || ''}
 					placeholder={placeholder}
 					type={type}
 					className="input"
--- src/components/cart/ShoppingList.tsx
+++ src/components/cart/ShoppingList.tsx
@@ -1,110 +1,61 @@
+import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks';
 import Button from '../Button';
-import Input from '../Input';
 import './css/shoppingList.css';
 const ShoppingList = () => {
 	const removeProduct = () => {};
+	const addQuantity = () => {};
+	const restQuantity = () => {};
+	const { productsList } = useCart();
+
 	return (
 		<section className="shoppingList">
 			<h2>Your shopping cart</h2>
-			<ul>
-				<li>
-					<img
-						src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-						alt=""
-					/>
-					<p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-					<section className="priceContent">
-						<Input type="number" />
-						<div>
-							<span>$2500</span>
-							<span>$223 / per Item</span>
-						</div>
-					<Button onClick={removeProduct}>Remove</Button>
-					</section>
-				</li>
-			
-				<li>
-					<img
-						src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-						alt=""
-					/>
-					<p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-					<section className="priceContent">
-						<Input type="number" />
-						<div>
-							<span>$2500</span>
-							<span>$223 / per Item</span>
-						</div>
-					<Button onClick={removeProduct}>Remove</Button>
-					</section>
-				</li>
-			
-				<li>
-					<img
-						src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-						alt=""
-					/>
-					<p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-					<section className="priceContent">
-						<Input type="number" />
-						<div>
-							<span>$2500</span>
-							<span>$223 / per Item</span>
-						</div>
-					<Button onClick={removeProduct}>Remove</Button>
-					</section>
-				</li>
-			
-				<li>
-					<img
-						src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-						alt=""
-					/>
-					<p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-					<section className="priceContent">
-						<Input type="number" />
-						<div>
-							<span>$2500</span>
-							<span>$223 / per Item</span>
-						</div>
-					<Button onClick={removeProduct}>Remove</Button>
-					</section>
-				</li>
-			
-				<li>
-					<img
-						src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-						alt=""
-					/>
-					<p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-					<section className="priceContent">
-						<Input type="number" />
-						<div>
-							<span>$2500</span>
-							<span>$223 / per Item</span>
-						</div>
-					<Button onClick={removeProduct}>Remove</Button>
-					</section>
-				</li>
-			
-				<li>
-					<img
-						src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-						alt=""
-					/>
-					<p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-					<section className="priceContent">
-						<Input type="number" />
-						<div>
-							<span>$2500</span>
-							<span>$223 / per Item</span>
-						</div>
-					<Button onClick={removeProduct}>Remove</Button>
-					</section>
-				</li>
-			
+			{productsList.length > 0 ? (
+				<ul>
+					{productsList.map(({ id, img, price, name, quantity }) => (
+						<li key={id}>
+							<img
+								src={img}
+								alt={name}
+							/>
+							<p>{name}</p>
 
-			</ul>
+							<section className="quantityContent">
+								<Button onClick={restQuantity}>-</Button>
+								<span>{quantity}</span>
+								<Button onClick={addQuantity}>+</Button>
+							</section>
+
+							<section className="priceContent">
+								<div>
+									<span>${price * quantity}</span>
+									<span>${price} / per Item</span>
+								</div>
+								<Button onClick={removeProduct}>Remove</Button>
+							</section>
+						</li>
+					))}
+				</ul>
+			) : (
+				<section className="shoppingListEmpty">
+					<h1>Your Cart is empty</h1>
+
+					<Link to={'/'}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 15 15">
+							<path
+								fill="currentColor"
+								d="M8.293 2.293a1 1 0 0 1 1.414 0l4.5 4.5a1 1 0 0 1 0 1.414l-4.5 4.5a1 1 0 0 1-1.414-1.414L11 8.5H1.5a1 1 0 0 1 0-2H11L8.293 3.707a1 1 0 0 1 0-1.414Z"
+							/>
+						</svg>
+						Go to store
+					</Link>
+				</section>
+			)}
 		</section>
 	);
 };

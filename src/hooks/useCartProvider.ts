47c9ab3938d5497b55fae167002423f37b11155a
jean carlos <jeancarlos3102@gmail.com>
2024-04-13 14:27:06 -0400

    split the logic from  provider

---
+++ src/hooks/useCartProvider.ts
@@ -0,0 +1,90 @@
+import { useNavigate } from 'react-router-dom';
+import { Cart } from '../types/cart';
+import { notyf } from '../libs/noty/noty';
+import { confirmDelete, confirmSuccess } from '../libs/sweetalert2/sweetalert2';
+import { useState } from 'react';
+
+const useCartProvider = () => {
+console.log(`🚀 ------------ useCartProvider:`, useCartProvider)
+
+	const [productsList, setProductsList] = useState<Cart[]>([]);
+	const [quantity, setQuantity] = useState(0);
+	const navigate = useNavigate();
+
+	const addToCart = (product: Cart) => {
+		setQuantity((prevValue) => prevValue + 1);
+		if (productsList.some((item) => item.id === product.id)) {
+			const newProduct = productsList.map((item) =>
+				item.id === product.id
+					? { ...item, quantity: item.quantity + product.quantity }
+					: item
+			);
+
+			setProductsList(newProduct);
+		} else {
+			setProductsList([...productsList, product].sort((a, b) => a.index - b.index).reverse());
+		}
+
+		notyf.success('Added to Cart');
+	};
+
+	const subtractQuantity = (id: number) => {
+		const newProduct = productsList.map((item) => {
+			if (item.id === id) {
+				if (item.quantity < 1) {
+					notyf.error('Quantity must be greater than zero');
+					return item;
+				}
+				return { ...item, quantity: item.quantity - 1 };
+			}
+			return item;
+		});
+
+		const filterProduct = newProduct.filter((item) =>
+			item.quantity > 0 ? item : ''
+		);
+		setProductsList(filterProduct);
+		setQuantity((prevValue) => prevValue - 1);
+	};
+
+	const addQuantity = (id: number) => {
+		const newProduct = productsList.map((item) =>
+			item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+		);
+		setProductsList(newProduct);
+		setQuantity((prevValue) => prevValue + 1);
+	};
+
+	const removeProduct = async (id: number, quantityValue: number) => {
+		const confirm = await confirmDelete('product');
+		if (!confirm) return;
+
+		if (productsList.some((item) => item.id === id)) {
+			const newProduct = productsList.filter((item) => {
+				setQuantity(quantity - quantityValue);
+				return item.id !== id;
+			});
+			setProductsList(newProduct);
+		}
+	};
+
+	const confirmPurchase = async () => {
+		const confirm = await confirmSuccess({ title: 'Make the purchase' });
+		if (confirm) {
+			setProductsList([]);
+			navigate('/');
+		}
+	};
+
+	return {
+		subtractQuantity,
+		addQuantity,
+		productsList,
+		addToCart,
+		quantity,
+		removeProduct,
+		confirmPurchase,
+	};
+};
+
+export default useCartProvider;
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,90 +1,26 @@
-import { useState } from 'react';
 import { CartContext } from '../contexts/CarContext';
-import { Cart } from '../types/cart';
-import { notyf } from '../libs/noty/noty';
-import { confirmDelete, confirmSuccess } from '../libs/sweetalert2/sweetalert2';
-import { useNavigate } from 'react-router-dom';
+import useCartProvider from '../hooks/useCartProvider';
 
 type CartProviderType = {
 	children: React.ReactNode;
 };
 
 export const CartProvider = ({ children }: CartProviderType) => {
-	const [cart, setCart] = useState<Cart[]>([]);
-	const [quantity, setQuantity] = useState(0);
-	const navigate = useNavigate();
-
-	const addToCart = (product: Cart) => {
-		setQuantity((prevValue) => prevValue + 1);
-		if (cart.some((item) => item.id === product.id)) {
-			const newProduct = cart.map((item) =>
-				item.id === product.id
-					? { ...item, quantity: item.quantity + product.quantity }
-					: item
-			);
-
-			setCart(newProduct);
-		} else {
-			setCart([...cart, product].sort((a, b) => a.index - b.index).reverse());
-		}
-
-		notyf.success('Added to Cart');
-	};
-
-	const subtractQuantity = (id: number) => {
-		const newProduct = cart.map((item) => {
-			if (item.id === id) {
-				if (item.quantity < 1) {
-					notyf.error('Quantity must be greater than zero');
-					return item;
-				}
-				return { ...item, quantity: item.quantity - 1 };
-			}
-			return item;
-		});
-
-		const filterProduct = newProduct.filter((item) =>
-			item.quantity > 0 ? item : ''
-		);
-		setCart(filterProduct);
-		setQuantity((prevValue) => prevValue - 1);
-	};
-
-	const addQuantity = (id: number) => {
-		const newProduct = cart.map((item) =>
-			item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-		);
-		setCart(newProduct);
-		setQuantity((prevValue) => prevValue + 1);
-	};
-
-	const removeProduct = async (id: number, quantityValue: number) => {
-		const confirm = await confirmDelete('product');
-		if (!confirm) return;
-
-		if (cart.some((item) => item.id === id)) {
-			const newProduct = cart.filter((item) => {
-				setQuantity(quantity - quantityValue);
-				return item.id !== id;
-			});
-			setCart(newProduct);
-		}
-	};
-
-	const confirmPurchase = async () => {
-		const confirm = await confirmSuccess({ title: 'Make the purchase' });
-		if (confirm) {
-			setCart([]);
-			navigate('/');
-		}
-	};
-
+	const {
+		addQuantity,
+		subtractQuantity,
+		productsList,
+		addToCart,
+		quantity,
+		removeProduct,
+		confirmPurchase,
+	} = useCartProvider();
 	return (
 		<CartContext.Provider
 			value={{
 				subtractQuantity: subtractQuantity,
 				addQuantity: addQuantity,
-				productsList: cart,
+				productsList: productsList,
 				addToCart: addToCart,
 				quantity: quantity,
 				removeProduct: removeProduct,

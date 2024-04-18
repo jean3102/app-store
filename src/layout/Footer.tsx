cc4685fba2c87897da5ed4e454c7356ed7387553
jean carlos <jeancarlos3102@gmail.com>
2024-04-18 19:20:03 -0400

    add fixed position and create footer element

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,10 +1,13 @@
-
+import '../styles/layout/footer.css';
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<section className="footer">
+			<p>©2024. All rights Reserved.</p>
+			<p>
+				Made by <b>Jean Carlos Luciano</b> in React JS
+			</p>
+		</section>
+	);
+};
 
-export default Footer
+export default Footer;
